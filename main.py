051e93d0eae942329cbfbbce5c49db88474b01ac
Martin Rampanelli <martin.rampanelli@student.uclouvain.be>
2022-03-21 12:43:52 +0100

    corrections

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ R = C/2
 beta = L/R
 gamma = 1.3
 
-
+sdf q
 def Vc():
-    return ((math.pi/D ^ 2)/4)*2*R
+    return ((math.pi/D ** 2)/4)*2*R
 
 
 def W(rpm):
@@ -33,31 +33,30 @@ def Qtot():
 
 
 def V(theta):
-    zp = R*(1 - math.cos(theta) + beta
-            - math.sqrt(beta ^ 2 - (math.sin(theta)) ^ 2))
+    zp = R*(1 - np.cos(theta) + beta - np.sqrt(beta**2 - (np.sin(theta))**2))
     return Vc()/2 * zp + Vc()/(tau - 1)
 
 
 def Q(theta, thetaC, deltaThetaC):
-    resultat = Qtot()*0.5*(1 - math.cos(math.pi*((theta - thetaC)/deltaThetaC)))
+    resultat = Q*0.5*(1 - np.cos(np.pi*((theta - thetaC)/deltaThetaC)))
     return resultat
 
 
 def dVdTheta(theta):
-    return Vc()*0.5*(math.sin(theta)+(beta ^ 2-(math.sin(theta)) ^ 2) ^ (-0.5) * math.cos(theta) * math.sin(theta))
+    return Vc()*0.5*(np.sin(theta)+(beta ** 2-(np.sin(theta))**2)**(-0.5) * np.cos(theta) * np.sin(theta))
 
 
 def dQDTheta(theta, thetaC, deltaThetaC):
-    resultat = (Q()*math.pi/(2*deltaThetaC)) * \
-                math.sin(math.pi*((theta-thetaC)/deltaThetaC))
+    resultat = (Q(theta, thetaC, deltaThetaC)*np.pi/(2*deltaThetaC)
+                ) * np.sin(math.pi*((theta-thetaC)/deltaThetaC))
     return resultat
 
 
 def p(rpm, s, theta, thetaC, deltaThetaC):
 
     def model(p, theta_model):
-        dpdTheta = -gamma * (p/V())*dVdTheta(theta) + (gamma - 1) * \
-                             1/V() * dQDTheta(theta, thetaC, deltaThetaC)
+        dpdTheta = -gamma * (p/V(theta))*dVdTheta(theta) + (gamma - 1) * \
+                             1/V(theta) * dQDTheta(theta, thetaC, deltaThetaC)
         return dpdTheta
 
     # initial condition
@@ -106,12 +105,12 @@ def myfunc(rpm, s, theta, thetaC, deltaThetaC):
     #zp = R*(1 - cos(theta) + beta - sqrt(beta^2 - (sin(theta))^2))
     #V_output = Vc()/2 * zp + Vc()/(tau - 1)
 
-    V_output = V()
+    V_output = V(theta)
 
     # Fp
     p_theta = p(rpm, s, theta, thetaC, deltaThetaC)
-    fraction = ((math.pi*(D ^ 2))/4)*p_theta
-    RW2Cos = R*(W(rpm)) ^ 2*math.cos(theta)
+    fraction = ((math.pi*(D ** 2))/4)*p_theta
+    RW2Cos = R*(W(rpm)) ** 2*math.cos(theta)
 
     F_pied_output = fraction - mpiston*RW2Cos
     F_tete_output = -fraction+(mpiston+mbielle)*RW2Cos
@@ -125,3 +124,15 @@ def myfunc(rpm, s, theta, thetaC, deltaThetaC):
     t = 1
 
     return (V_output, Q_output, F_pied_output, F_tete_output, p_output, t)
+
+
+rpm = 4106
+s = 2.5
+theta = np.linspace(-180, 180, 1000)
+thetaC = 21
+deltaThetaC = 48
+
+v, q, fpied, ftete, p, dq = myfunc(rpm, s, theta, thetaC, deltaThetaC)
+
+plt.plot(theta, v)
+plt.plot(theta, dVdTheta(theta))
